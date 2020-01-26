89db4b0e74eae4ec622682d683428aec2ed4782f
Simon Burton <burt76@outlook.com>
2020-01-26 21:47:10 +0000

    change to rtepo outfput

--- Archimedes.Api.Repository/Controllers/RepositoryController.cs
+++ Archimedes.Api.Repository/Controllers/RepositoryController.cs
@@ -11,7 +11,7 @@ namespace Archimedes.Fx.Api.Repository.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "repository.api", "repository.api" };
+            return new string[] { "repository.api.rep", "repository.api.repo" };
         }
 
         // GET: api/Repository/5
