7e11bf1af10a786e25acea0b8311cfb02536a4f3
Simon Burton <burt76@outlook.com>
2020-10-07 15:52:40 +0100

    s

--- Archimedes.Api.Repository/Core/Domain/Market.cs
+++ Archimedes.Api.Repository/Core/Domain/Market.cs
@@ -9,14 +9,13 @@ namespace Archimedes.Api.Repository
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         [Key]
         public int Id { get; set; }
-
         public string Name { get; set; }
         public int Interval { get; set; }
         public string TimeFrame { get; set; }
         public string Granularity { get; set; }
         public bool Active { get; set; }
-        public DateTime MaxDate { get; set; }
         public DateTime MinDate { get; set; }
+        public DateTime MaxDate { get; set; }
         public int Quantity { get; set; }
         public DateTime LastUpdated { get; set; }
     }
