ecfe72d27e80dd4048467c2e0ffdb20c5aa060d7
Simon Burton <burt76@outlook.com>
2021-01-06 16:25:36 +0000

    added trade stuff

---
+++ Archimedes.Api.Repository/Migrations/20210106162503_pricelevels.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Archimedes.Api.Repository.Migrations
+{
+    public partial class pricelevels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "Trades",
+                type: "decimal(18,5)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<double>(
+                name: "RiskReward",
+                table: "Trades",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Trades");
+
+            migrationBuilder.DropColumn(
+                name: "RiskReward",
+                table: "Trades");
+        }
+    }
+}
